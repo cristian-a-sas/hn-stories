681b2df00c3459b90aee09bc85b58660213bf5dd
Cristian Adrian Sas <crsa@dr.dk>
2022-02-21 07:52:44 +0100

    Add mock data

---
+++ __mockData__/stories.ts
@@ -0,0 +1,87 @@
+const storiesMock = [
+  {
+    id: 30405127,
+    title: "The professor who beat roulette",
+    url: "https://thehustle.co/professor-who-beat-roulette/",
+    time: 1645359375,
+    score: 4,
+    by: "pseudolus",
+  },
+  {
+    id: 30390442,
+    title:
+      "Biggest concentration of military forces in Europe now since Cold War",
+    url: "https://www.aljazeera.com/news/2022/2/18/g7-says-ready-for-serious-dialogue-with-russia-on-ukraine-live",
+    time: 1645215074,
+    score: 12,
+    by: "cryptography",
+  },
+  {
+    id: 30382568,
+    title:
+      "Ask HN: How can I secure my AWS instance's SSH access if I have no static IP?",
+    time: 1645163939,
+    score: 13,
+    by: "gkrishna",
+  },
+  {
+    id: 30392926,
+    title: "Ask HN: How do you implement audit trail for a product?",
+    time: 1645229036,
+    score: 15,
+    by: "_448",
+  },
+  {
+    id: 30390473,
+    title: "The Trip to Rose Cottage",
+    url: "https://granta.com/the-trip-to-rose-cottage/",
+    time: 1645215291,
+    score: 20,
+    by: "robbybaron",
+  },
+  {
+    id: 30400494,
+    title: "Caxa: Package Node.js applications into executable binaries",
+    url: "https://github.com/leafac/caxa",
+    time: 1645304475,
+    score: 26,
+    by: "thunderbong",
+  },
+  {
+    id: 30388633,
+    title:
+      "Fed approves rules banning its officials from trading stocks, bonds and crypto",
+    url: "https://www.cnbc.com/2022/02/18/fed-approves-rules-banning-its-officials-from-trading-stocks-bonds-and-also-cryptocurrencies.html",
+    time: 1645205892,
+    score: 36,
+    by: "geox",
+  },
+  {
+    id: 30384179,
+    title: "How the Fehmarnbelt Tunnel is being built [video]",
+    url: "https://www.youtube.com/watch?v=zf7C5DiEVpA",
+    time: 1645180818,
+    score: 177,
+    by: "blutack",
+  },
+  {
+    id: 30404587,
+    title:
+      "Google Drive users stung by macOS '.DS_Store' copyright infringement issue",
+    url: "https://appleinsider.com/articles/22/02/19/google-drive-users-stung-by-macos-dsstore-copyright-infringement-issue",
+    time: 1645351332,
+    score: 209,
+    by: "laktak",
+  },
+  {
+    id: 30387866,
+    title:
+      "Windows 11 Pro will soon require a Microsoft Account during initial setup",
+    url: "https://www.theverge.com/2022/2/18/22940517/windows-11-pro-require-microsoft-account-internet-connection",
+    time: 1645202186,
+    score: 375,
+    by: "SoapSeller",
+  },
+];
+
+export default storiesMock;
---
+++ __mockData__/users.ts
@@ -0,0 +1,44 @@
+const usersMock = [
+  {
+    id: "pseudolus",
+    karma: 114067,
+  },
+  {
+    id: "cryptography",
+    karma: 478,
+  },
+  {
+    id: "gkrishna",
+    karma: 15,
+  },
+  {
+    id: "_448",
+    karma: 386,
+  },
+  {
+    id: "robbybaron",
+    karma: 364,
+  },
+  {
+    id: "thunderbong",
+    karma: 14582,
+  },
+  {
+    id: "geox",
+    karma: 4861,
+  },
+  {
+    id: "blutack",
+    karma: 805,
+  },
+  {
+    id: "laktak",
+    karma: 3702,
+  },
+  {
+    id: "SoapSeller",
+    karma: 2052,
+  },
+];
+
+export default usersMock;
